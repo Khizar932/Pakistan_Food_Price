ec476ea259c70b7bed97c1c39b5af50a6168f179
Khizar932 <106827346+Khizar932@users.noreply.github.com>
2022-07-11 14:21:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,21 @@
-#Import main library
-import numpy as np
 
-#Import Flask modules
-from flask import Flask, request, render_template
+from flask import Flask
 
-#Import pickle to save our regression model
-import pickle 
+from api.config import get_logger
 
-#Initialize Flask and set the template folder to "template"
-app = Flask(__name__, template_folder = 'template')
 
-#Open our model 
-model = pickle.load(open('model.pkl','rb'))
+_logger = get_logger(logger_name=__name__)
 
-#create our "home" route using the "index.html" page
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-#Set a post method to yield predictions on page
-@app.route('/', methods = ['POST'])
-def predict():
-    
-    #obtain all form values and place them in an array, convert into integers
-    int_features = [int(x) for x in request.form.values()]
-    #Combine them all into a final numpy array
-    final_features = [np.array(int_features)]
-    #predict the price given the values inputted by user
-    prediction = model.predict(final_features)
-    
-    #Round the output to 2 decimal places
-    output = round(prediction[0], 2)
-    
-    #If the output is negative, the values entered are unreasonable to the context of the application
-    #If the output is greater than 0, return prediction
-    if output < 0:
-        return render_template('index.html', prediction_text = "Predicted Price is negative, values entered not reasonable")
-    elif output >= 0:
-        return render_template('index.html', prediction_text = 'Predicted Price of the house is: ${}'.format(output))   
+def create_app(*, config_object) -> Flask:
+    """Create a flask app instance."""
 
-#Run app
-if __name__ == "__main__":
-    app.run(debug=True)
+    flask_app = Flask('ml_api')
+    flask_app.config.from_object(config_object)
 
+    # import blueprints
+    from api.controller import prediction_app
+    flask_app.register_blueprint(prediction_app)
+    _logger.debug('Application instance created')
+
+    return flask_app
